de1879833e5a5c71cb6668c1a952a7b65b3e359f
Chris Lacey <ChrisLacey89@gmail.com>
2019-04-19 19:35:05 -0400

    ready to start!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -20,6 +20,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <span class="badge badge-primary">Primary</span>
       </div>
     );
   }
