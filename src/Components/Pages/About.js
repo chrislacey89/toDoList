a4472d3dd5ccaf7071e8937dc930772778ddef8d
Chris Lacey <ChrisLacey89@gmail.com>
2020-01-22 13:34:15 -0500

    updated about section

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 function About() {
   return (
     <React.Fragment>
-      <h1>About</h1>
-      <p>This is a Todo List App. Will revisit this section later</p>
+      <h1 align='center'>About</h1>
+
+      <Grid container justify='center' spacing={0}>
+        <Grid item xs={10}>
+          <subtitle1>
+            This is a full stack Todo List app. It is powered by MongoDB and
+            Node JS on the back end and React and Material UI on the front end.
+            It includes a basic feature set of user authentication along with
+            CRUD functionality to persist user generated data. This is still a
+            working project and it may receive additional updates.
+          </subtitle1>
+        </Grid>
+      </Grid>
+      <div></div>
     </React.Fragment>
   );
 }
